5e4cc5590c6626643013bc4c1552032a867c68df
JurgenKlopp <JurgenKlopp@gmail.com>
2024-03-05 23:36:31 +0500

    Appspec added

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 3306:3306  --name my_mysql_container125 -e MYSQL_ROOT_PASSWORD=danyal123 --network=go_net mysql:latest
-docker run -d -p 4040:4040  --name appcontainer_goapp --env-file ./.env --network=go_net danyalharoon/project1_danyal_goapp:v1.0
+#docker run -d -p 3306:3306  --name my_mysql_container125 -e MYSQL_ROOT_PASSWORD=danyal123 --network=go_net mysql:latest
+#docker run -d -p 4040:4040  --name appcontainer_goapp --env-file ./.env --network=go_net danyalharoon/project1_danyal_goapp:v1.0
+
+pwd 
